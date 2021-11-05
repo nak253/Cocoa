081ea0d0d394f8e303746fef21ed394cb0fbce9c
nak253 <78344294+nak253@users.noreply.github.com>
2021-11-05 11:22:21 +0900

    11654번 완료

---
+++ src/day5/Num11654.java
@@ -0,0 +1,14 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Num11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int i = str.charAt(0);
+
+        System.out.println(i);
+    }
+}
