2d8d6ee237c376a374b794dc1171cc3b17b718a2
nak253 <78344294+nak253@users.noreply.github.com>
2021-11-05 11:22:03 +0900

    1008번 완료

---
+++ src/day5/Num1008.java
@@ -0,0 +1,14 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Num1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a =sc.nextInt();
+        int b= sc.nextInt();
+
+        double result = (double) a/b;
+        System.out.println(result);
+    }
+}
