f483ab2c2aa43cd7c1d6d5f10f660e7f72627971
nak253 <nwon4413@naver.com>
2021-12-01 16:22:05 +0900

    다리건너기게임 구현3
    1. 플레이어 순번 랜덤으로 지정하는 메서드 구현

--- src/steppingStone/SteppingStones.java
+++ src/steppingStone/SteppingStones.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class SteppingStones {
 
+    private final int TOTAL_PLAYER_NUM=9;
+
     private Scanner scanner;
     private Validator validator;
 
     private Bridge bridge;
     private Player player;
+
+    private int playerNum;
 
     public void init() {
         scanner = new Scanner(System.in);
@@ -17,6 +21,7 @@ public class SteppingStones {
     }
 
     public void start() {
+        setPlayerNum();
         while (!bridge.isStop()){
             bridge.printBridge();
             bridge.countPlayer();
@@ -26,6 +31,11 @@ public class SteppingStones {
         System.out.println("게임이 종료되었습니다.");
     }
 
+    private void setPlayerNum() {
+        playerNum = (int)(Math.random()*TOTAL_PLAYER_NUM)+1;
+        System.out.println("당신은 "+playerNum+"번 참가자입니다.");
+    }
+    
     private void crossTheBridge() {
         while (bridge.isPassStep(player)){
             bridge.printBridge();
